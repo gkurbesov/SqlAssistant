148d6b5c9a21edff605eba45104a41fc4875de5c
Gennady <ltrg67@gmail.com>
2021-02-25 15:26:29 +0300

    Added comments

--- SqlAssistant/ISqlAssistant.cs
+++ SqlAssistant/ISqlAssistant.cs
@@ -6,12 +6,31 @@ using System.Threading.Tasks;
 
 namespace SqlAssistant
 {
+    /// <summary>
+    /// Base assistant interface
+    /// </summary>
     public interface ISqlAssistant
     {
+        /// <summary>
+        /// Get new connection with custom config
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         IDbConnection GetConnection(ISqlConfig value);
+        /// <summary>
+        /// Get new connection
+        /// </summary>
+        /// <returns></returns>
         IDbConnection GetConnection();
+        /// <summary>
+        /// Open and return new connection
+        /// </summary>
+        /// <returns></returns>
         IDbConnection GetOpenConnection();
+        /// <summary>
+        /// Open and return new connection
+        /// </summary>
+        /// <returns></returns>
         Task<IDbConnection> GetOpenConnectionAsync();
-
     }
 }
