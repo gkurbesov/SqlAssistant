d4f2658cce7a8eab5bf95504ddf6dbb745c2665f
Gennady <ltrg67@gmail.com>
2021-02-25 17:23:06 +0300

    Added sample

---
+++ SqlAssistant.Sample/Mysql/MySqlDatabase.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace SqlAssistant.Sample.Mysql
+{
+    public class MySqlDatabase : MySql.MySqlAssistant
+    {
+        public MySqlDatabase(ISqlConfig config)
+            : base(config) { }
+
+        public DataSet Query(string query)
+        {
+            using var connection = GetOpenConnection() as MySqlConnection;
+            using MySqlCommand command = new MySqlCommand(query, connection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            DataSet data = new DataSet();
+            adapter.Fill(data);
+            return data;
+        }
+    }
+}
---
+++ SqlAssistant.Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SqlAssistant.MySql;
+using SqlAssistant.Sample.Mysql;
+
+namespace SqlAssistant.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new MySqlConfig("localhost", "3306")
+                .SetDatabase("DATABASE_NAME")
+                .SetUser("USER")            // set your login
+                .SetPassword("PASSWORD");    // set your password
+            var database = new MySqlDatabase(config);
+            var dataSet = database.Query("SELECT * FROM user"); // request for example
+        }
+    }
+}
